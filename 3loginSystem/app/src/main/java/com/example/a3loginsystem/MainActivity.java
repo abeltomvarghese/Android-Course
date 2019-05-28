8f563e520cec6bd0157dfbdd0f85129e86e7d3b2
unknown <abeltomvarghese1998@gmail.com>
2019-05-28 22:48:00 +0100

    my attempt

---
+++ 3loginSystem/app/src/main/java/com/example/a3loginsystem/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a3loginsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void clickFunction (View view) {
+        EditText userName = (EditText) findViewById(R.id.userName);
+        EditText passWord = (EditText) findViewById(R.id.password);
+        Log.i("Username", userName.getText().toString());
+        Log.i("Password", passWord.getText().toString());
+
+    }
+}
