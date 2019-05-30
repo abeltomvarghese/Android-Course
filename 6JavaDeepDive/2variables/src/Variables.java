f128db232a238b4d6ec2283a98fe0b910eb3c272
unknown <abeltomvarghese1998@gmail.com>
2019-05-30 17:39:04 +0100

    recap all java basics

---
+++ 6JavaDeepDive/2variables/src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main (String[] args) {
+        int age = 34;
+        double decimalNum = 3.4;
+        long bigNum = 34223;   //Bigger than integer
+        boolean valid = false;
+        String name = "Bruce Wayne";
+        System.out.println("My name is: " + name + " age: " + bigNum);
+
+        /*The length of the string */
+        String countryName = "Czechoslovakia";
+        System.out.println(countryName.length());
+
+        String firstName = "Bruce";
+        String surname = "Wayne";
+        System.out.println(firstName.length() + surname.length());
+    }
+}
