9430355f86cc89a104971567777f3b5f15f71006
unknown <abeltomvarghese1998@gmail.com>
2019-05-30 21:45:26 +0100

    fixed the random num bug

--- 7HigherLowerApp/app/src/main/java/com/example/a7higherlowerapp/MainActivity.java
+++ 7HigherLowerApp/app/src/main/java/com/example/a7higherlowerapp/MainActivity.java
@@ -9,25 +9,30 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        generateRandomNum();
+    }
+
+    public void generateRandomNum() {
+        Random rand = new Random();
+        answer = rand.nextInt(20) + 1;
     }
 
     public void calculateChance(View view) {
         EditText guess = (EditText) findViewById(R.id.editTextGuess);
         int inputGuess = Integer.parseInt(guess.getText().toString());
-        Random rand = new Random();
-        int answer = rand.nextInt(2);
-        answer++;
         if (inputGuess > answer) {
-            Toast.makeText(this,"lower",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "lower", Toast.LENGTH_SHORT).show();
         } else if (inputGuess < answer) {
-            Toast.makeText(this,"higher",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "higher", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this,"Well Done!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Well Done!", Toast.LENGTH_SHORT).show();
+            generateRandomNum();
         }
     }
 }
