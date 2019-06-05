f3cc118d648df6410bdf1d240e2b717cd987a4db
unknown <abeltomvarghese1998@gmail.com>
2019-06-05 17:51:02 +0100

    fixed the drawn issue

--- 11Connect3/app/src/main/java/com/example/a11connect3/MainActivity.java
+++ 11Connect3/app/src/main/java/com/example/a11connect3/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     int[] gameState = {2,2,2,2,2,2,2,2,2};
     int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     boolean hasWon = false;
+    boolean drawn = false;
+
     public void dropIn (View view) {
         ImageView counter = (ImageView) view;
 
@@ -33,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
                 counter.setImageResource(R.drawable.red);
                 activePlayer = 0;
             }
-
+            Button playAgain = (Button) findViewById(R.id.btnPlay);
+            TextView message = (TextView) findViewById(R.id.tvWinner);
             counter.animate().translationYBy(1500).rotation(3600).setDuration(400);
             for (int[] position : winningPositions) {
                 if (gameState[position[0]] == gameState[position[1]] && gameState[position[1]] == gameState[position[2]] && gameState[position[0]] != 2) {
@@ -44,13 +47,24 @@ public class MainActivity extends AppCompatActivity {
                     } else {
                         winner = "red";
                     }
-                    Button playAgain = (Button) findViewById(R.id.btnPlay);
-                    TextView message = (TextView) findViewById(R.id.tvWinner);
+
                     message.setText(winner + " has won");
                     playAgain.setVisibility(View.VISIBLE);
                     message.setVisibility(View.VISIBLE);
 
                 }
+            }
+            int drawnCounter = 0;
+            for (int index = 0; index < gameState.length; index++) {
+                if (gameState[index] != 2) {
+                    drawnCounter++;
+                }
+            }
+            if (drawnCounter == 9) {
+                drawn = true;
+                message.setText("DRAW!");
+                playAgain.setVisibility(View.VISIBLE);
+                message.setVisibility(View.VISIBLE);
             }
         }
     }
@@ -69,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             gameState[index] = 2;
         }
         activePlayer = 0;
-
+        drawn = false;
         hasWon = false;
     }
     @Override
